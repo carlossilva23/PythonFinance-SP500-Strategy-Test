76106cb90b07db23fd684d16e0c916c95cefb86d
carlossilva23 <carlossilva23@arizona.edu>
2025-09-04 16:32:12 -0700

    Set Up for Day 2

--- src/fetch_data.py
+++ src/fetch_data.py
@@ -1,3 +1,7 @@
 import yfinance as yf
 
+portfolio =['EA', 'GRMN', 'GIS', 'VLO', 'GS', 'BSX', 'UBER', 'FSLR', 'ECL', 'IRM', 'PNW']
+
+for stocks in portfolio: 
+    ticker = yf.download((stocks), period='1mo', auto_adjust=True)
 
