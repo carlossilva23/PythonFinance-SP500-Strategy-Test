43328c831c156145d5b129d873d3b01943411a41
carlossilva23 <carlossilva23@arizona.edu>
2025-09-09 16:39:38 -0700

    Backtest file was started, skeleton code was created

--- src/backtest.py
+++ src/backtest.py
@@ -1,3 +1,34 @@
-def simulate_portfolio(ticker_data): 
-    cash = 15,000
+import pandas as pd
 
+# ticker_data is a dictionary of dataframes ticker symbol: dataframe
+portfolio_value = pd.DataFrame(columns =['Date', 'Portfolio Value'])
+trade_log = pd.DataFrame(columns = ['Date', 'Ticker', 'Action', 'Price', 'Shares', 'Cash After', 
+                                    'Total Holdings', 'Portfolio Value'])
+cash = 11,000
+
+def initialize_portfolio(ticker_data): 
+    holdings = {}
+    for ticker in ticker_data.keys(): 
+        holdings[ticker] = 0
+    
+def simulate_portfolio(ticker_data): 
+    control = next(iter(ticker_data.values()))
+    dates = control.index
+
+    for date in dates: 
+        for ticker in ticker_data.items(): 
+            # if ticker[buy signal] == true and cash > 1000
+                # shares = 1000 // price
+                # holdings[ticker] = shares
+                # remaining = 1000 (shares * price)
+                # cash += remaining
+                # trade_log[date] = date
+                # trade_log[action] = buy
+                # trade_log[ticker] = ticker
+                # append all trade_log values 
+            # else if ticker[sell signal] == true and holdings[ticker] > 0
+                # cash += (holdings[ticker] * price)
+                # holdings[ticker] = 0
+                # trade_log append 
+        # portfolio value appends (updated)
+    
--- src/main.py
+++ src/main.py
@@ -5,15 +5,14 @@ import visualize
 
 def main(): 
     portfolio =['EA', 'GRMN', 'GIS', 'VLO', 'GS', 'BSX', 'UBER', 'FSLR', 'ECL', 'IRM', 'PNW']
-    portfolio_data = []
+    portfolio_data = {}
     for stock in portfolio: 
         individual_data = fetch_data.fetch_stock(stock)
         calculate_data.calculate_short_MA(individual_data)
         calculate_data.calculate_long_MA(individual_data)
         calculated_data = calculate_data.crossover(individual_data)
-        portfolio_data.append(calculated_data)
-        return portfolio_data
-    
+        portfolio_data[stock] = calculated_data
+        backtest.simulate_portfolio(portfolio_data) 
 main()
 
 
