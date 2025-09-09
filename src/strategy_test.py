585d1456435ee7aa49c540f2267b7c261fded528
carlossilva23 <carlossilva23@arizona.edu>
2025-09-09 09:02:51 -0700

    Fixed Buy/Sell Logic, Added matplotlib to env for visual representation, Portfolio File Created

--- src/strategy_test.py
+++ src/strategy_test.py
@@ -12,20 +12,18 @@ def calculate_long_MA(ticker_data):
 def crossover(ticker_data): 
     switch_signal = ticker_data['Short MA'] > ticker_data['Long MA']
     ticker_data['Switch'] = switch_signal
+    ticker_data['Prev Bool'] = ticker_data['Switch'].shift(1)
     
 
 def date_tracker(ticker_data): 
-    buy_signal = (ticker_data['Switch'].diff() == 1)
-    sell_signal = (ticker_data['Switch'].diff() == -1)
-    buy_dates = ticker_data.loc[buy_signal]
-    sell_dates = ticker_data.loc[sell_signal]
-    print(buy_dates.index)
-    print(sell_dates.index)
-
+    buy_signal = ticker_data[(ticker_data['Prev Bool'] == True) & (ticker_data['Switch'] == False)]
+    sell_signal = ticker_data[(ticker_data['Prev Bool'] == False) & (ticker_data['Switch'] == True)]
+    print(buy_signal.index)
+    print(sell_signal.index)
 
 
 def main(): 
-    ticker = yf.download('PNW', period='5y', auto_adjust=True)
+    ticker = yf.download('PNW', period='1y', auto_adjust=True)
     calculate_short_MA(ticker)
     calculate_long_MA(ticker)
     crossover(ticker)
