e592f93c8c53a83381b6df2b155104f00c7d4b51
carlossilva23 <carlossilva23@arizona.edu>
2025-09-10 23:20:23 -0700

    Backtest Rework Pt. 2

--- src/backtest.py
+++ src/backtest.py
@@ -20,29 +20,34 @@ def simulate_portfolio(ticker_data, holdings):
             buy_signal = ticker.loc[date, 'Buy Signal']
             sell_signal = ticker.loc[date, 'Sell Signal']
             price = ticker.loc[date, 'Close']
-            if (buy_signal == True) and (cash > 1000):
+            if (sell_signal == True) and (holdings[symbol] > 0):
+                shares = holdings[symbol]
+                cash += (shares * price)
+                holdings[symbol] = 0
+                log_trade(trade_log, date, symbol, "Sell", price, shares, cash, holdings, ticker_data)
+            elif (buy_signal == True) and (cash > 1000):
                 shares = 1000 // price
                 holdings[symbol] = shares
                 remaining = 1000 - (shares * price)
                 cash += remaining
-                log_trade(trade_log, date, symbol, "Buy", price, shares, cash, holdings)
-            elif (sell_signal == True) and (holdings[symbol] > 0):
-                cash += (holdings[symbol] * ticker.loc[date, 'Close'])
-                holdings[symbol] = 0
-                
-
-
-            def log_trade(trade_log, date, symbol, action, price, shares, cash, holdings):
-                trade_log['Date'] = date
-                trade_log['Ticker'] = symbol
-                trade_log['Action'] = action
-                trade_log['Price'] = price
-                trade_log['Shares'] = shares
-                trade_log['Cash After'] = cash
-                total_holdings = 0
-                for shares in holdings.values(): 
-                    total_holdings += shares
-                trade_log['Total Holdings'] = total_holdings
+                log_trade(trade_log, date, symbol, "Buy", price, shares, cash, holdings, ticker_data)
             
-        # portfolio value appends (updated)
+        
+                
+def log_trade(trade_log, date, symbol, action, price, shares, cash, holdings, ticker_data):
+    total_holdings = 0
+    for stock, holds in holdings.items(): 
+        total_holdings += holds
+    portfolio_value = cash + sum(holdings[stock] * ticker_data[stock].loc[date, "Close"])
+    trade_log.append({
+        "Date": date,
+        "Ticker": symbol,
+        "Action": action,
+        "Price": price,
+        "Shares": shares,
+        "Cash": cash,
+        "Holdings": total_holdings,
+        "Portfolio Value": portfolio_value,
+    })
+                
     
